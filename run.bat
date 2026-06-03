ae0cb49757043ab3fe33a36d1bfd3a6b8381e319
AlmogAtias <26almog@gmail.com>
2026-06-03 18:20:14 +0300

    Fix run.bat to use bundled JDK 11 instead of system java

--- run.bat
+++ run.bat
@@ -1,7 +1,11 @@
 @echo off
-:: Run KeyLogger (after running build.bat once)
+:: Run KeyLogger using the bundled JDK 11
 if not exist keylogger.jar (
     echo keylogger.jar not found -- run build.bat first.
     pause & exit /b 1
 )
-java -jar keylogger.jar
+
+set "JAVA_BIN=%~dp0jdk\jdk-11.0.31+11\bin\java.exe"
+if not exist "%JAVA_BIN%" set "JAVA_BIN=java"
+
+"%JAVA_BIN%" -jar "%~dp0keylogger.jar"
